d00c4c78ab312b47e201bb986298a61030f8e493
Source-Code-007 <utsho926@gmail.com>
2023-12-23 11:56:04 +0600

    multer added and testing file uploading with multer

--- admin/adminRouter.js
+++ admin/adminRouter.js
@@ -2,11 +2,27 @@ const express = require('express');
 
 const adminRouter = express.Router()
 
+// Testing middleware
+const adminLogger = (req, res, next)=>{
+    console.log('admin router fired!');
+    next()
+}
+
+
+// Both are same for use application level middleware
+// adminRouter.use(adminLogger)
+adminRouter.all('*', adminLogger)
+
+
 adminRouter.get('/', (req, res)=> {
-    res.send('This is admin home!')
+    // res.send('This is admin home!')
+    // throw new Error('error happened')
+    res.send(a)
 })
 adminRouter.get('/pending-doctors', (req, res)=> {
     res.send('This is pending doctors route!')
 })
 
-module.exports = (adminRouter)
+
+
+module.exports = adminRouter
--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const express = require('express');
 const cors = require('cors')
 const app = express()
 const adminRouter = require('./admin/adminRouter')
-
-app.use(cors())
-app.use(express.json())
-app.use('/admin', adminRouter) // using sub app for admin router
+const multer = require('multer')
 
 const port = process.env.PORT || 4000
 require('dotenv').config()
 
 
+app.use(cors())
+app.use(express.json())
+app.use('/admin', adminRouter) // using sub app for admin router
 
-
+// prepare the final multer upload object
+const fileUploadDest = './upload'
+const upload = multer({
+    dest: fileUploadDest
+})
 
 app.enable('case sensitive routing');
 
@@ -25,6 +29,38 @@ app.get('/test', (req, res) => {
     res.send('This is test')
 })
 
+// upload single files
+app.post('/upload-profile', upload.single('profile-picture'), (req, res)=> {
+    res.send('testing file uploaded!')
+})
+// upload multiple files #3 is max file
+app.post('/upload-profile', upload.array('profile-pictures', 3), (req, res)=> {
+    res.send('testing files uploaded!')
+})
+// upload multiple files for different files
+app.post('/upload-profile', upload.fields([
+    {'profile-photo': 4},
+    {'medicine-photo': 3}
+]), (req, res)=> {
+    res.send('testing files uploaded!')
+})
+
+
+
+// 404 Error handling
+app.use((req, res, next)=>{
+    res.status(404).send('Requested URL was not found!')
+})
+
+
+// Error handling middleware
+app.use((err, req, res, next)=>{
+    if(err.message){
+        res.status(500).send({message:err.message})
+    } else{
+        res.status(500).send("There was an error!")
+    }
+})
 
 app.listen(port, () => {
     console.log(`Doc Eye server is running at ${port}!!`);
