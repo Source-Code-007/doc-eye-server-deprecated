cb3e48a875df57d2f2e7763e740e971565222ae0
Source-Code-007 <utsho926@gmail.com>
2023-12-26 11:01:54 +0600

    multer file upload storage managemenet and upload obj config done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const app = express()
 const adminRouter = require('./admin/adminRouter')
 const multer = require('multer')
+const path = require('path')
 
 const port = process.env.PORT || 4000
 require('dotenv').config()
@@ -12,19 +13,47 @@ app.use(cors())
 app.use(express.json())
 app.use('/admin', adminRouter) // using sub app for admin router
 
+
 // prepare the final multer upload object
 const fileUploadDest = './upload'
+
+// define storage 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, fileUploadDest)
+    },
+    filename: (req, file, cb) => {
+        // const modifiedFileNameArr = file.originalname.split('.');
+        // const fileExt = modifiedFileNameArr.at(-1)
+        const fileExt = path.extname(file.originalname)
+        const modifiedName = `${file.originalname.replace(fileExt, '').toLowerCase().split('-').join('')}-${Date.now()}${fileExt}`
+        console.log(modifiedName);
+        cb(null, modifiedName)
+    }
+})
+
+// file upload config and operation
 const upload = multer({
-    dest: fileUploadDest,
+    storage: storage,
     limits: {
         fileSize: 1000000 // 1MB - By bytes
-    }, 
-    fileFilter: (req, file, callback)=>{
+    },
+    fileFilter: (req, file, callback) => {
         console.log(file);
-        if(file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg'){
-            callback(null, true) // first param - error null, second param - permit true
-        } else{
-            callback(new Error('Only .jpg, .jpeg and .png format allowed!'))
+        if (file.fieldname === 'medicine-photos') {
+            if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+                callback(null, true) // first param - error null, second param - permit true
+            } else {
+                callback(new Error('Only .jpg, .jpeg and .png format allowed!'))
+            }
+        } else if (file.fieldname === 'doctor-documents') {
+            if (file.mimetype === 'application/pdf') {
+                callback(null, true) // first param - error null, second param - permit true
+            } else {
+                callback(new Error('Only .pdf format allowed!'))
+            }
+        } else {
+            callback(new Error('File upload err!!!'))
         }
     }
 })
@@ -43,38 +72,39 @@ app.get('/test', (req, res) => {
 
 // ----upload files----
 // upload single files 
-app.post('/upload-profile', upload.single('profile-picture'), (req, res)=> {
-    res.send('testing file uploaded!')
-})
+// app.post('/upload-profile', upload.single('profile-picture'), (req, res)=> {
+//     res.send('testing file uploaded!')
+// })
 // // upload multiple files #3 is max file
 // app.post('/upload-profile', upload.array('profile-pictures', 3), (req, res)=> {
 //     res.send('testing files uploaded!')
 // })
 // // upload multiple files for different files
-// app.post('/upload-profile', upload.fields([
-//     {name: 'profile-photo', maxCount: 4},
-//     {name: 'medicine-photo', maxCount: 3}
-// ]), (req, res)=> {
-//     res.send('testing files uploaded!')
-// })
+app.post('/upload-profile', upload.fields([
+    { name: 'profile-photos', maxCount: 4 },
+    { name: 'doctor-documents', maxCount: 3 },
+    { name: 'medicine-photos', maxCount: 3 },
+]), (req, res) => {
+    res.send('testing files uploaded!')
+})
 
 
 
 // ----404 Error handling----
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     res.status(404).send('Requested URL was not found!')
 })
 
 
 // Error handling middleware
-app.use((err, req, res, next)=>{
-    if(err.message){
-        if(err instanceof multer.MulterError){
-            res.status(500).send({message: 'There was an file upload related error!'})
-        } else{
-            res.status(500).send({message:err.message})
+app.use((err, req, res, next) => {
+    if (err.message) {
+        if (err instanceof multer.MulterError) {
+            res.status(500).send({ message: 'There was an file upload related error!' })
+        } else {
+            res.status(500).send({ message: err.message })
         }
-    } else{
+    } else {
         res.status(500).send("There was an error!")
     }
 })
