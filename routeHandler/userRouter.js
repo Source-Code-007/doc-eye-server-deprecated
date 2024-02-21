d821018941e690d35dd10a321ad6398c1a503ff9
Source-Code-007 <utsho926@gmail.com>
2024-02-21 20:24:09 +0600

    initial  signup and signin functionality added

--- routeHandler/userRouter.js
+++ routeHandler/userRouter.js
@@ -1,12 +1,44 @@
-const express = require('express') 
-const mongoose = require('mongoose') 
+const express = require('express')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 const userRouter = express.Router()
 const userSchema = require('../schemas/userSchema')
 const User = new mongoose.model('user', userSchema)
 
 
-userRouter.post('/sign-up', (req, res)=> {
-    res.send('Signup route triggered')
+userRouter.post('/signup', async (req, res) => {
+    try {
+        const { name, email, password, role } = req.body
+        const hashPass = await bcrypt.hash(password, 10) //Password encrypted using bcrypt
+        const newUser = new User({ name, email, password: hashPass, role })
+        await newUser.save()
+        if (newUser) {
+            res.status(200).send({ message: 'User created successfully' })
+        } else {
+            res.status(500).send({ message: 'There was a server side error' })
+        }
+    } catch (e) {
+        res.status(500).send({ message: `There was a server side error` })
+    }
+})
+userRouter.post('/signin', async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await User.findOne({ email: email }, { __v: 0 })
+        if (user) {
+            const isValidPass = await bcrypt.compare(password, user?.password) //Password encrypted using bcrypt
+            if (isValidPass) {
+                res.status(200).send({ message: 'User found successfully', user })
+            } else {
+                res.status(500).send({ message: 'Authentication failed!' })
+            }
+        }
+        else {
+            res.status(500).send({ message: 'Authentication failed!' })
+        }
+    } catch (e) {
+        res.status(500).send({ message: 'Authentication failed!' })
+    }
 })
 
 
