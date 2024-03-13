5e69a1d16e696f66d5b328b67d85c5b0e6e69f1d
Source-Code-007 <utsho926@gmail.com>
2024-03-13 13:16:13 +0600

    default avatar added confitionally based on gender

--- models/Users.js
+++ models/Users.js
@@ -5,7 +5,15 @@ const userSchema = mongoose.Schema({
     name: { type: String, required: true },
     phone: { type: String, required: true},
     gender: { type: String, required: true, enum:['male', 'female', 'other']},
-    avatar: { type: String, required: true, default: 'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp' },
+    avatar: { type: String, required: true, default: function(){
+        if(this.gender === 'male'){
+            return 'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp'
+        } else if(this.gender === 'female'){
+            return 'https://w7.pngwing.com/pngs/284/973/png-transparent-china-girl-cartoon-free-s-of-girl-on-girl-child-face-black-hair.png'
+        } else if(this.gender === 'other'){
+            return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzY9VLmrZN4lZahZ_IjBwKFRhsoCEOytfWvg&usqp=CAU'
+        }
+    } },
     email: {
         type: String,
         lowercase: true,
