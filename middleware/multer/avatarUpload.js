fbea3184d7722730a4b6facebb37f4672b71c57a
Source-Code-007 <utsho926@gmail.com>
2024-03-16 13:22:10 +0600

    upload folder modified

--- middleware/multer/avatarUpload.js
+++ middleware/multer/avatarUpload.js
@@ -3,6 +3,7 @@ const singleUploader = require("../../utilities/singleUploader");
 function avatarUpload(req, res, next) {
     const upload = singleUploader('./upload/avatar', ['image/png', 'image/jpg', 'image/jpeg'], 'Only .jpg, .jpeg and .png format allowed!')
 
+    
     // Call the middleware function
     upload.any()(req, res, err => {
         if (err) {
