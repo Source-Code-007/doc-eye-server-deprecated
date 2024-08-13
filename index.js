c41fecaa08b9c0e6f07bf81edf20ec0385f8d352
Ahashan Habib Utsho <utsho926@gmail.com>
2024-08-13 23:18:30 +0600

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const path = require('path')
 const app = express()
 
 
-
-
 // Internal imports
 const adminRouter = require('./api/routeHandler/adminRouter');
 const doctorRouter = require('./api/routeHandler/doctorRouter');
